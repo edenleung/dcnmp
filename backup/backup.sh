5e79a79dcc9fa0fff1c25d3227fcc953666c6ba9
XiaoDi <31346973+xiaodit@users.noreply.github.com>
2019-04-19 20:18:59 +0800

    Update backup.sh

--- backup/backup.sh
+++ backup/backup.sh
@@ -2,7 +2,7 @@
 cd /home/backup
 git pull
 
-mysqldump -h mysql56 -u root -p'1234' project > /home/backup/project-$(date +"%Y-%m-%d").sql
+mysqldump -h mysql56 -u root -p'1234' database > /home/backup/project-$(date +"%Y-%m-%d").sql
 git add -A .
 git commit -m update
 git push
