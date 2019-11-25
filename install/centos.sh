3de29d6b04b7427ee3659ec89c3fef344df31e77
XiaoDi <31346973+xiaodit@users.noreply.github.com>
2019-11-25 17:38:41 +0800

    Create centos.sh

---
+++ install/centos.sh
@@ -0,0 +1,11 @@
+#! /bin/bash
+
+sudo yum install -y yum-utils device-mapper-persistent-data lvm2
+
+sudo yum-config-manager --add-repo http://mirrors.aliyun.com/docker-ce/linux/centos/docker-ce.repo
+
+sudo yum makecache fast
+
+sudo yum -y install docker-ce
+
+sudo service docker start
